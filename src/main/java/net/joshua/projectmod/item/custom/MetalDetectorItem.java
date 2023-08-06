c823bf437fb1d548107aa92774dcbb312fc192a0
Joshua Anderson <myasscantpass@gmail.com>
2023-08-06 18:03:09 -0500

    started to add metal detector

---
+++ src/main/java/net/joshua/projectmod/item/custom/MetalDetectorItem.java
@@ -0,0 +1,36 @@
+package net.joshua.projectmod.item.custom;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
+
+public class MetalDetectorItem extends Item {
+    public MetalDetectorItem(Properties pProperties) {
+        super(pProperties);
+    }
+
+    @Override
+    public InteractionResult useOn(UseOnContext pContext) {
+        if (pContext.getLevel().isClientSide()) {
+            BlockPos positionClicked = pContext.getClickedPos();
+            Player PLAYER = pContext.getPlayer();
+            boolean foundBlock = false;
+            for (int i = 0; i <= positionClicked.getY() + 64; i++) {
+                BlockState state = pContext.getLevel().getBlockState(positionClicked.below(i));
+
+                if (isValubleBlock(state)) {
+
+                }
+            }
+    }
+    return InteractionResult.SUCCESS;
+    }
+
+    private boolean isValubleBlock(BlockState state) {
+        return state.is(Blocks.IRON_ORE);
+    }
+}
